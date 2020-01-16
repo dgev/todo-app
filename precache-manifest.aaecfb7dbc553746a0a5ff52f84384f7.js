69e1ba65b748ab3a59897824a079a3a2c33bad4a
dgev <diana_gevorgyan2@edu.aua.am>
2020-01-17 02:13:44 +0400

    Updates

---
+++ precache-manifest.aaecfb7dbc553746a0a5ff52f84384f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8231b2ebcdd304f918764115a0d9bc05",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "b2c10143de759c07144e",
+    "url": "/todo-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d58886dd0c7d00e12545",
+    "url": "/todo-app/static/js/2.0ef9dbf0.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/todo-app/static/js/2.0ef9dbf0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2c10143de759c07144e",
+    "url": "/todo-app/static/js/main.16b0c08b.chunk.js"
+  },
+  {
+    "revision": "b66ecab6fc712c6d22a7",
+    "url": "/todo-app/static/js/runtime-main.e77e13dd.js"
+  }
+]);
